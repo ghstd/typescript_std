1f59fabea7d47aafee46ff1346ec268d3076ed54
AntonRm <tworkn1@gmail.com>
2023-03-12 22:52:09 +0200

    add temp code in app.ts

--- app.js
+++ app.js
@@ -1,47 +1,31 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
+// string, number, boolean, null, undefined, any, void, unknown
+// tupl, enum, union types('|' - объединение, '&' - пересечение)
+// readonly для tupl, array (пример альтернативной записи ReadonlyArray<string>)
+const o = {
+    a: 1,
+    b: 2,
+    c: 3
 };
-let TsClass = class TsClass {
-    constructor() {
-        this.num = 1;
-    }
-    classLog() {
-        return this.num;
-    }
-    cL() {
-        console.log(this.num);
-    }
+const res = Object.keys(o);
+const v = { a: 2 };
+const part = {
+    'a': 2,
+    'c': 3
 };
-TsClass = __decorate([
-    fabric(234),
-    fnDecor,
-    tsDecor
-], TsClass);
-function tsDecor(target) {
-    console.log(target);
-    return class extends target {
-        constructor() {
-            super(...arguments);
-            this.num = 123;
-        }
-    };
+const Ll = {
+    a: 'str',
+    b: 1,
+    c: 2
+};
+function get(param) { }
+get(Ll);
+const skls = ['a', 1];
+Ll.a = 'qwe';
+function enFn(value) {
+    return value;
 }
-function fnDecor(target) {
-    console.log(target);
-}
-console.log(new TsClass().classLog());
-function fabric(valuse) {
-    return function (target) {
-        return class extends target {
-            constructor() {
-                super(...arguments);
-                this.num = valuse;
-            }
-        };
-    };
-}
-console.log(new TsClass().classLog());
+// console.log(enFn(testEnum))
+// console.log(testEnum)
+// console.log(nEnum)
+const vv = 0 /* nEnum.a */;
--- app.ts
+++ app.ts
@@ -1,4 +1,37 @@
+// string, number, boolean, null, undefined, any, void, unknown
+// tupl, enum, union types('|' - объединение, '&' - пересечение)
+// readonly для tupl, array (пример альтернативной записи ReadonlyArray<string>)
 
+// number
+// string
+// boolean
+// object
+// array
+// tupl
+// enum
+// union
+// intersection
+// literal type
+// type (alias)
+// interface
+
+// сужение типов (type narrowing):
+// typeof id === 'number'
+// Array.isArray(arr)
+// 'key' in object (или object.hasOwnProperty) (наличие определенного свойста в объекте)
+
+// явная задача нужного типа (кастование):
+//  someProp as 'type here'
+
+// 1. параметр функции может принимать более широкий тип, чем описано
+// 2. в объявлении типа tupl можно исаользовать spred-оператор (...), напр.:
+// const skls: [string, number, ...boolean[]] = ['a', 1, true, true];
+// 3. enum - принимает числа и строки, по-умолчанию числовой, может быть гетерогенным.
+// после компиляции enum - это самовыз. функция, однако const enum становится одной константой.
+// 4. literal type - переменная, объявленная через 'const', или явно:
+// function myFetch(param: 10 | 20, method: 'post' | 'get') {}
+
+// ============================================
 
 interface I {
 	'a': number,
@@ -16,9 +49,6 @@ type Res = 'a'[]
 
 const res: Res = Object.keys(o) as (keyof Pick<I, 'a'>)[]
 
-
-
-
 const v: Pick<I, 'a'> = { a: 2 }
 
 const part: Partial<Omit<I, 'b'>> = {
@@ -26,35 +56,56 @@ const part: Partial<Omit<I, 'b'>> = {
 	'c': 3
 }
 
+// ============================================
 
+interface Ii {
+	a: string
+	b: number
+}
 
+const Ll = {
+	a: 'str',
+	b: 1,
+	c: 2
+}
 
+function get(param: Ii) { }
 
+get(Ll)
 
+const skls: readonly [string, number, ...boolean[]] = ['a', 1]
 
+Ll.a = 'qwe'
 
+// console.log(Ll)
 
+interface enTest {
+	a: 'aa',
+	b: 'bb',
+	c: 'cc'
+}
 
+const enum testEnum {
+	a = 'aa',
+	b = 'bb',
+	c = 'cc'
+}
 
+const enum nEnum {
+	a,
+	b,
+	c
+}
 
+function enFn(value: enTest) {
+	return value
+}
 
+// console.log(enFn(testEnum))
+// console.log(testEnum)
+// console.log(nEnum)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const vv = nEnum.a;
 
 
 
